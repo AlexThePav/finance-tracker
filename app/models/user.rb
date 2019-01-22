8c9afc19b4b2d5d934e2773d0b0a0fedbd55d9d0
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2019-01-22 23:16:45 +0200

    add delete friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,9 @@ class User < ApplicationRecord
     users.reject { |user| user.id == self.id }
   end
   
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
   
   
 end
